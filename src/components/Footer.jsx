28d57f63b9df0c1f20dc4645f2f53f5411ea3c67
Vinicius Montanari <viniciusc29@gmail.com>
2024-06-08 18:34:36 -0300

    feat: mobile layout done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,10 +5,22 @@ import './Footer.scss';
 const Footer = () => (
   <footer className="footer">
     <div className="links">
-      <a href="#contact">Contact</a>
-      <a href="#privacy">Privacy Policy</a>
-      <a href="#terms">Terms of Service</a>
+      <a href="#facebook"><img src='https://i.ibb.co/Fzn9WFK/facebook-com.png' alt='fb'/></a>
+      <a href="#linkedin"><img src='https://i.ibb.co/9h55rPC/www-linkedin-com.png' alt='In'/></a>
+      <a href="#instagram"><img src='https://i.ibb.co/BTXb7gp/www-instagram-com.png' alt='insta'/></a>
+      <a href="#youtube"><img src='https://i.ibb.co/3sbjDfD/www-youtube-com.png' alt='youtube'/></a>
+      <img className='pagamentos' src='https://i.ibb.co/KrTY9py/accepted-payment-methods-meio-de-pagamentos.png' alt='pgtos'/>
     </div>
+    <div className='powered'>
+        <div className='vtex'>
+          <p>Powered by</p>
+          <img src='https://i.ibb.co/bmqVD5b/logo-footer.png' alt='vtex'/>
+        </div>
+        <div className='maeztra'>
+          <p>Developed by</p>
+          <img src='https://i.ibb.co/YTfjTFm/2019-05-Maeztra-Logo-cor.png' alt='maeztra'/>
+        </div>
+      </div>
   </footer>
 );
 
