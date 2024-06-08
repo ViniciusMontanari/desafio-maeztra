365f4a01ad8cd4a5f25378ce6ef1175b4a897a36
Vinicius Montanari <viniciusc29@gmail.com>
2024-06-08 16:41:08 -0300

    feat: lorem section done

--- src/components/LoremText.jsx
+++ src/components/LoremText.jsx
@@ -9,7 +9,7 @@ const LoremText = () => (
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Interdum mauris posuere fusce proin mattis. Hendrerit cras ut nunc enim dictum. 
                 Mattis proin ut quis donec sed eget nulla. Vel mi ut mauris integer. Nibh sagittis in lobortis sed cursus condimentum velit pharetra. Amet luctus ut vulputate scelerisque .
             </p>
-            <img src="https://i.imgur.com/1vLCv01.png" />
+            <img src="https://i.imgur.com/1vLCv01.png" alt="img"/>
         </div>
     </section>
 )
