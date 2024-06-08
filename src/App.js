af5480e42ee3fe85dbfe467510294ac7a3ed814f
Vinicius Montanari <viniciusc29@gmail.com>
2024-06-08 16:04:04 -0300

    feat: shelf and lorem sections done

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Header from './components/Header';
 import Banner from './components/Banner';
 import Tips from './components/Tips';
 import Partnership from './components/Partnership';
+import Shelf from './components/Shelf';
+import LoremText from './components/LoremText';
 import Footer from './components/Footer';
 import NotificationBar from './components/NotificationBar';
 
@@ -16,6 +18,8 @@ function App() {
       <Banner />
       <Tips />
       <Partnership />
+      <Shelf />
+      <LoremText />
       <Footer />
     </div>
   );
---
+++ src/components/LoremText.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import './LoremText.scss'
+
+const LoremText = () => (
+    <section className="lorem">
+        <div className="lorem-infos">
+            <h1>Lorem ipsum</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Interdum mauris posuere fusce proin mattis. Hendrerit cras ut nunc enim dictum. 
+                Mattis proin ut quis donec sed eget nulla. Vel mi ut mauris integer. Nibh sagittis in lobortis sed cursus condimentum velit pharetra. Amet luctus ut vulputate scelerisque .
+            </p>
+            <img src="https://i.imgur.com/1vLCv01.png" />
+        </div>
+    </section>
+)
+
+export default LoremText;
