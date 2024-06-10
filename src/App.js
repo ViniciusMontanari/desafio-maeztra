4a2fc50d013265eead691231930943c16dc91d1b
Vinicius Montanari <viniciusc29@gmail.com>
2024-06-09 23:45:34 -0300

    feat: chalenge done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import Header from './components/Header';
-import Banner from './components/Banner';
-import Tips from './components/Tips';
-import Partnership from './components/Partnership';
-import Shelf from './components/Shelf';
-import LoremText from './components/LoremText';
-import Newsletter from './components/Newsletter';
-import BottomInfoMenu from './components/BottomInfoMenu';
-import Footer from './components/Footer';
-import Popup from './components/Popup';
-import PopupDesktop from './components/PopupDesktop';
-import DesktopMenu from './components/DesktopMenu';
-import NotificationBar from './components/NotificationBar';
+import Header from './components/Header/Header';
+import Banner from './components/Banner/Banner';
+import Tips from './components/Tips/Tips';
+import Partnership from './components/Partnership/Partnership';
+import Shelf from './components/Shelf/Shelf';
+import LoremText from './components/LoremText/LoremText';
+import Newsletter from './components/Newsletter/Newsletter';
+import BottomInfoMenu from './components/BottomInfoMenu/BottomInfoMenu';
+import Footer from './components/Footer/Footer';
+import Popup from './components/Popup/Popup';
+import PopupDesktop from './components/PopupDesktop/PopupDesktop';
+import DesktopMenu from './components/DesktopMenu/DesktopMenu';
+import NotificationBar from './components/NotificationBar/NotificationBar';
 
 import './App.css';
 
